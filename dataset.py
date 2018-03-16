3fe160021cdebbfb951a71706f5020b8deb5be2f
Maksim Petrov <maksimallist@gmail.com>
2018-03-16 15:39:40 +0300

    refactor: write dataset class and dataset test;

--- dataset.py
+++ dataset.py
@@ -5,81 +5,24 @@ from sklearn.model_selection import train_test_split
 
 
 class Dataset(object):
-    def __init__(self, data, seed=None, split=True, splitting_proportions=None,
-                 *args, **kwargs):
+
+    def __init__(self, data, seed=None, classes_description=None, *args, **kwargs):
 
         rs = random.getstate()
         random.seed(seed)
         self.random_state = random.getstate()
         random.setstate(rs)
 
-        if splitting_proportions is None:
-            self.splitting_proportions = [0.1, 0.1]
-        else:
-            self.splitting_proportions = splitting_proportions
+        self.classes_description = classes_description
+        self.data = dict()
+        self.data['train'] = data.get('train')
+        self.data['test'] = data.get('test')
+        self.data['valid'] = data.get('valid')
 
-        if not split:
-            self.train = data.get('train', [])
-            self.test = data.get('test', [])
-            try:
-                self.valid = data.get('valid', [])
-                self.data = {'train': {'base': self.train},
-                             'test': {'base': self.test},
-                             'valid': {'base': self.valid},
-                             'all': {'base': self.train + self.test}}
-            except KeyError:
-                self.data = {'train': {'base': self.train},
-                             'test': {'base': self.test},
-                             'all': {'base': self.train + self.test}}
-        else:
-            self.train, self.valid, self.test = self.split_data(data)
-            self.data = {'train': {'base': self.train},
-                         'test': {'base': self.test},
-                         'valid': {'base': self.valid},
-                         'all': {'base': self.train + self.test}}
+        if (self.data['train'] is None) and (self.data['valid'] is None) and (self.data['test'] is None):
+            self.data['base'] = data
 
-        self.data['classes'] = data['class'].unique()  # np.array
-
-    def batch_generator(self, batch_size: int, data_type: str = 'train', stage: str = 'base') -> Generator:
-        """This function returns a generator, which serves for generation of raw (no preprocessing such as tokenization)
-         batches
-        Args:
-            batch_size (int): number of samples in batch
-            data_type (str): can be either 'train', 'test', or 'valid'
-            stage (str): mod
-        Returns:
-            batch_gen (Generator): a generator, that iterates through the part (defined by data_type) of the dataset
-        """
-        data = self.data[data_type][stage]
-        data_len = len(data)
-        order = list(range(data_len))
-
-        rs = random.getstate()
-        random.setstate(self.random_state)
-        random.shuffle(order)
-        self.random_state = random.getstate()
-        random.setstate(rs)
-
-        # for i in range((data_len - 1) // batch_size + 1):
-        #     yield list(zip(*[data[o] for o in order[i * batch_size:(i + 1) * batch_size]]))
-        for i in range((data_len - 1) // batch_size + 1):
-            o = order[i * batch_size:(i + 1) * batch_size]
-            yield list((list(data['request'][o]), list(data['class'][o])))
-
-    def iter_all(self, data_type: str = 'train', stage: str = 'base') -> Generator:
-        """
-        Iterate through all data. It can be used for building dictionary or
-        Args:
-            data_type (str): can be either 'train', 'test', or 'valid'
-            stage (str):
-        Returns:
-            samples_gen: a generator, that iterates through the all samples in the selected data type of the dataset
-        """
-        data = self.data[data_type][stage]
-        for x, y in zip(data['request'], data['class']):
-            yield (x, y)
-
-    def _split_data(self, splitting_proportions, field_to_split, splitted_fields):
+    def simple_split(self, splitting_proportions, field_to_split, splitted_fields, delete_parent=True):
         data_to_div = self.data[field_to_split].copy()
         data_size = len(self.data[field_to_split])
         for i in range(len(splitted_fields) - 1):
@@ -88,9 +31,14 @@ class Dataset(object):
                                                                           len(data_to_div) -
                                                                           int(data_size * splitting_proportions[i]))
         self.data[splitted_fields[-1]] = data_to_div
-        return True
 
-    def split_data(self, dataset):
+        if delete_parent:
+            a = self.data.pop(field_to_split)
+            del a
+
+        return self
+
+    def split(self, splitting_proportions=None, delete_parent=True):
 
         dd = dict()
         cd = dict()
@@ -98,7 +46,16 @@ class Dataset(object):
         valid = list()
         test = list()
 
-        for x, y in zip(dataset['request'], dataset['class']):
+        if splitting_proportions is None:
+            splitting_proportions = [0.1, 0.1]
+
+        if self.data.get('base', []) is not None:
+            dataset = self.data['base']
+        else:
+            raise ValueError("You dataset don't contains 'base' key. If You want to split a specific part dataset,"
+                             "please use .simple_split method.")
+
+        for x, y in zip(dataset['request'], dataset['report']):
             if y not in dd.keys():
                 dd[y] = list()
                 cd[y] = 0
@@ -108,20 +65,20 @@ class Dataset(object):
                 dd[y].append((x, y))
                 cd[y] += 1
 
-        if type(self.splitting_proportions) is list:
-            assert len(self.splitting_proportions) == 2
-            assert type(self.splitting_proportions[0]) is float
+        if type(splitting_proportions) is list:
+            assert len(splitting_proportions) == 2
+            assert type(splitting_proportions[0]) is float
 
             valid_ = dict()
             test_ = dict()
 
             for x in dd.keys():
-                num = int(cd[x] * self.splitting_proportions[0])
+                num = int(cd[x] * splitting_proportions[0])
                 valid_[x] = random.sample(dd[x], num)
                 [dd[x].remove(t) for t in valid_[x]]
 
             for x in dd.keys():
-                num = int(cd[x] * self.splitting_proportions[1])
+                num = int(cd[x] * splitting_proportions[1])
                 test_[x] = random.sample(dd[x], num)
                 [dd[x].remove(t) for t in test_[x]]
         else:
@@ -144,20 +101,71 @@ class Dataset(object):
                 n.append(x[0])
                 c.append(x[1])
 
-        train = pd.DataFrame({'request': utrain,
-                              'class': ctrain})
-        valid = pd.DataFrame({'request': uvalid,
-                              'class': cvalid})
-        test = pd.DataFrame({'request': utest,
-                             'class': ctest})
+        self.data['train'] = pd.DataFrame({'request': utrain, 'report': ctrain})
+        self.data['valid'] = pd.DataFrame({'request': uvalid, 'report': cvalid})
+        self.data['test'] = pd.DataFrame({'request': utest, 'report': ctest})
 
-        return train, valid, test
+        if delete_parent:
+            a = self.data.pop('base', [])
+            del a
 
-    def _merge_data(self, fields_to_merge):
-        data = self.data.copy()
-        new_name = [s + '_' for s in fields_to_merge]
-        data[new_name] = []
-        for name in fields_to_merge:
-            data[new_name] += self.data[name]
-        self.data = data
-        return True
+        return self
+
+    def iter_batch(self, batch_size: int, data_type: str = 'base') -> Generator:
+        """This function returns a generator, which serves for generation of raw (no preprocessing such as tokenization)
+         batches
+        Args:
+            batch_size (int): number of samples in batch
+            data_type (str): can be either 'train', 'test', or 'valid'
+        Returns:
+            batch_gen (Generator): a generator, that iterates through the part (defined by data_type) of the dataset
+        """
+        data = self.data[data_type]
+        data_len = len(data)
+        order = list(range(data_len))
+
+        rs = random.getstate()
+        random.setstate(self.random_state)
+        random.shuffle(order)
+        self.random_state = random.getstate()
+        random.setstate(rs)
+
+        # for i in range((data_len - 1) // batch_size + 1):
+        #     yield list(zip(*[data[o] for o in order[i * batch_size:(i + 1) * batch_size]]))
+        for i in range((data_len - 1) // batch_size + 1):
+            o = order[i * batch_size:(i + 1) * batch_size]
+            yield list((list(data['request'][o]), list(data['report'][o])))
+
+    def iter_all(self, data_type: str = 'base') -> Generator:
+        """
+        Iterate through all data. It can be used for building dictionary or
+        Args:
+            data_type (str): can be either 'train', 'test', or 'valid'
+        Returns:
+            samples_gen: a generator, that iterates through the all samples in the selected data type of the dataset
+        """
+        data = self.data[data_type]
+        for x, y in zip(data['request'], data['report']):
+            yield (x, y)
+
+    def merge_data(self, fields_to_merge, delete_parent=True, new_name=None):
+        if new_name is None:
+            new_name = '_'.join([s for s in fields_to_merge])
+
+        if set(fields_to_merge) <= set(self.data.keys()):
+            fraims_to_merge = [self.data[s] for s in fields_to_merge]
+            self.data[new_name] = pd.concat(fraims_to_merge)
+        else:
+            raise KeyError('In dataset no such parts {}'.format(fields_to_merge))
+
+        if delete_parent:
+            a = [self.data.pop(x) for x in fields_to_merge]
+            del a
+
+        return self
+
+    def info(self):
+        information = dict(data_keys=list(self.data.keys()),
+                           classes_description=self.classes_description)
+
+        return information
