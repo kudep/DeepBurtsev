e3805864184ec08253308cfdcac92c06fa15993a
Maksim Petrov <maksimallist@gmail.com>
2018-03-28 14:24:52 +0300

    fix: bug with pipeline generator;

--- script/core/pipe_gen.py
+++ script/core/pipe_gen.py
@@ -11,6 +11,7 @@ from sklearn.linear_model import LogisticRegression
 from sklearn.svm import LinearSVC
 from sklearn.ensemble import RandomForestClassifier
 import json
+from itertools import product
 
 
 class GetCNN(BaseModel):
@@ -143,3 +144,132 @@ class PipeGen(object):
         model_type = np.random.choice(['neural', 'linear'])
         pipe = self.pipeline_gen(model_type=model_type)
         return pipe
+
+
+def genc(pipe, var):
+    keys_values = []
+    keys = list(var.keys())
+    for key in var.keys():
+        if key not in pipe.keys():
+            raise KeyError('Key "{}" not in  pipe.'.format(key))
+        else:
+            keys_values.append(var[key])
+
+    params_gen = product(*keys_values)
+    for params in params_gen:
+        for i, x in enumerate(params):
+            pipe[keys[i]] = x
+        yield pipe
+
+
+class PipelineGenerator(object):
+    def __init__(self):
+        self.vec_default = {'op_type': 'vectorizer', 'name': 'tf-idf vectorizer',
+                            'request_names': ['train', 'valid', 'test'],
+                            'new_names': ['train_vec', 'valid_vec', 'test_vec']}
+
+        # Concatenator
+        self.ops_dict = {'Speller': Speller,
+                         'Tokenizer': Tokenizer,
+                         'Lemmatizer': Lemmatizer,
+                         'TextConcatenator': TextConcat,
+                         'FasttextVectorizer': FasttextVectorizer,
+                         'tf-idf': TfidfVectorizer,
+                         'count': CountVectorizer,
+                         'CNN': CNN,
+                         'RandomForestClassifier': RandomForestClassifier,
+                         'LinearSVC': LinearSVC,
+                         'LogisticRegression': LogisticRegression,
+                         'LGBMClassifier': LGBMClassifier}
+
+        self.neural_struct = {'Speller': [False, True], 'Lemmatizer': [False, True], 'model': ['CNN']}
+        self.neural_pipe = OrderedDict(Speller=True,
+                                       Tokenizer=True,
+                                       Lemmatizer=True,
+                                       vectorizer='FasttextVectorizer',
+                                       model='CNN')
+
+        self.linear_struct = {'Speller': [False, True], 'Lemmatizer': [False, True],
+                              'vectorizer': ['tf-idf', 'count'],
+                              'model': ['LogisticRegression',
+                                        'RandomForestClassifier',
+                                        'LGBMClassifier',
+                                        'LinearSVC']}
+        self.linear_pipe = OrderedDict(Speller=True,
+                                       Tokenizer=True,
+                                       Lemmatizer=True,
+                                       TextConcatenator=True,
+                                       vectorizer='tf-idf',
+                                       model='LogisticRegression')
+
+    # generation
+    def pipeline_gen(self):
+        model_types = ['neural', 'linear']
+
+        for type_ in model_types:
+            if type_ == 'neural':
+                gen = genc(self.neural_pipe, self.neural_struct)
+                resulter = GetResult
+            elif type_ == 'linear':
+                gen = genc(self.linear_pipe, self.linear_struct)
+                resulter = GetResultLinear
+            else:
+                raise ValueError('')
+
+            for conf in gen:
+                if type_ == 'linear':
+                    conf['Tokenizer'] = conf['Lemmatizer']
+                    conf['TextConcatenator'] = conf['Lemmatizer']
+
+                pipeline_config = OrderedDict()
+                pipe = []
+                for key in conf.keys():
+                    if isinstance(conf[key], bool):
+                        path = './configs/ops/' + key + '.json'
+                        config = get_config(path)
+                        pipeline_config[str(key) + '_transformer'] = config
+                        pipe.append((self.ops_dict[key], config))
+                    elif isinstance(conf[key], str):
+                        if key == 'vectorizer':
+                            if conf[key] == 'FasttextVectorizer':
+                                config = get_config('./configs/ops/FasttextVectorizer.json')
+                                pipeline_config['FasttextVectorizer_vectorizer'] = config
+                                pipe.append((self.ops_dict[conf[key]], config))
+                            elif conf[key] == 'tf-idf':
+                                config = self.vec_default
+                                config['name'] = 'tf-idf'
+                                pipeline_config['tf-idf_vectorizer'] = config
+                                vec = sktransformer(self.ops_dict[conf[key]], config)
+                                pipe.append((vec,))
+                            elif conf[key] == 'count':
+                                config = self.vec_default
+                                config['name'] = 'count'
+                                pipeline_config['count_vectorizer'] = config
+                                vec = sktransformer(self.ops_dict[conf[key]], config)
+                                pipe.append((vec,))
+                            else:
+                                raise AttributeError('Vectorizer {} is not implemented yet.'.format(conf[key]))
+                        elif key == 'model':
+                            if conf[key] in ['LogisticRegression', 'LGBMClassifier',
+                                             'RandomForestClassifier', 'LinearSVC']:
+                                path = './configs/models/Linear/' + conf[key] + '.json'
+                                config = get_config(path)
+                                model = skmodel(self.ops_dict[conf[key]], config)
+                                pipeline_config[conf[key] + '_model'] = config
+                                pipe.append((model,))
+                            elif conf[key] == 'CNN':
+                                path = './configs/models/CNN/WCNN.json'
+                                config = get_config(path)
+                                WCNN = GetCNN(self.ops_dict[conf[key]], config)
+                                pipeline_config['WCNN_model'] = config
+                                pipe.append((WCNN,))
+                            else:
+                                raise ValueError('Model {} is not implemented yet.'.format(conf[key]))
+                        else:
+                            raise ValueError('Unexpected key value {}'.format(key))
+                    else:
+                        raise TypeError('It wrong dict, attribute of dicts must have a bool type or str,'
+                                        'but {} was found.'.format(type(conf[key])))
+                pipe.append((resulter,))
+#                 (pipe, pipeline_config)
+                yield (pipe, pipeline_config)
