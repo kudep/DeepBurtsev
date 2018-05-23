bb28c1f08ae087c04677f155ea4581cc7f499ba6
Maksim Petrov <maksimallist@gmail.com>
2018-05-23 14:16:15 +0300

    fix: some mini bugs;

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ from os.path import join
 from deepburtsev.core.pipelinemanager import PipelineManager
 from deepburtsev.core.transformers import FasttextVectorizer, ResultsCollector
 from deepburtsev.models.intent_classification.DCNN import DCNN
+from deepburtsev.models.intent_classification.WCNN import WCNN
 from deepburtsev.models.skmodels.linear_models import LinearRegression, LinearSVM, RandomForest
 from deepburtsev.core.sktransformers import Tfidf
 from deepburtsev.core.sktransformers import Count
@@ -11,21 +12,26 @@ from deepburtsev.core.sktransformers import Count
 
 # data prepare
 root = '/home/mks/projects/DeepBurtsev/'
-file_path = join(root, 'data', 'english', 'new_group', 'dataset.json')
+file_path = join(root, 'data', 'russian', 'sber_faq', 'sber_faq.json')
 with open(file_path, 'r') as f:
     dataset = json.load(f)
     f.close()
 
 # create structure for pipeline manager
-fasttext = FasttextVectorizer(request_names=['train', 'valid', 'test'],
-                              new_names=['train', 'valid', 'test'],
-                              dimension=100,
-                              model_path='./embeddings/wordpunct_tok_reddit_comments_2017_11_100.bin')
+names = ['train', 'val', 'test']
+fasttext = FasttextVectorizer(request_names=names,
+                              new_names=names,
+                              dimension=300,
+                              model_path='./embeddings/ft_native_300_ru_wiki_lenta_nltk_word_tokenize.bin')
 
-tfidf = Tfidf(request_names=['train', 'valid', 'test'], new_names=['train', 'valid', 'test'])
-count = Count(request_names=['train', 'valid', 'test'], new_names=['train', 'valid', 'test'])
+tfidf = Tfidf(request_names=names, new_names=names)
+count = Count(request_names=names, new_names=names)
 
-neural_struct = [fasttext, (DCNN, {'search': True, 'batch_size': 32, 'epochs': [3, 5, 8, 10, 12, 14, 16, 18, 20]}),
+neural_struct = [fasttext,
+                 [(WCNN(new_names=["pred_test", "pred_val"], predict_names=["test", "val"]),
+                   {'search': True, 'batch_size': [32, 64], 'epochs': 1}),
+                  (DCNN(new_names=["pred_test", "pred_val"], predict_names=["test", "val"]),
+                   {'batch_size': 32, 'epochs': 1, 'op_name': 'DCNN'})],
                  ResultsCollector]
 
 linear_struct = [[tfidf, count],
@@ -35,5 +41,5 @@ linear_struct = [[tfidf, count],
 neural_man = PipelineManager(dataset, neural_struct, 'skill_manager', target_metric='f1_macro')
 neural_man.run()
 
-# linear_man = PipelineManager(dataset, linear_struct, 'skill_manager', target_metric='f1_macro')
-# linear_man.run()
+linear_man = PipelineManager(dataset, linear_struct, 'skill_manager', target_metric='f1_macro')
+linear_man.run()
